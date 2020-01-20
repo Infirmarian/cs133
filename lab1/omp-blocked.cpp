0787ffcbda4efc7fc6c67b5ec499ba2efc90b6b3
Robert Geil <robert@rgeil.net>
2020-01-20 22:39:24 +0000

    got ~111 GFlops for omp blocked

--- lab1/omp-blocked.cpp
+++ lab1/omp-blocked.cpp
@@ -1,10 +1,35 @@
-// Header inclusions, if any...
-
+#include <cstring>
+#include <iostream>
 #include "gemm.h"
 
 // Using declarations, if any...
+const int finalSize = kI * kJ;
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
-                         float c[kI][kJ]) {
-  // Your code goes here...
+                         float c[kI][kJ])
+{
+  int k, j, ii, ik;
+  float sum;
+  const int BLOCK_SIZE = 32;
+  float aV;
+  float *cp = (float *)c;
+  std::memset(cp, 0, sizeof(float) * kJ * kI);
+#pragma omp parallel for private(k, j, ii, ik, sum) schedule(static, 32)
+  for (int i = 0; i < kI; i += BLOCK_SIZE)
+  {
+    for (k = 0; k < kK; k += BLOCK_SIZE)
+    {
+      for (ii = i; ii < i + BLOCK_SIZE; ++ii)
+      {
+        for (ik = k; ik < k + BLOCK_SIZE; ++ik)
+        {
+          //#pragma omp simd
+          for (j = 0; j < kJ; j++)
+          {
+            c[ii][j] += a[ii][ik] * b[ik][j];
+          }
+        }
+      }
+    }
+  }
 }
--- lab1/omp.cpp
+++ lab1/omp.cpp
@@ -1,10 +1,25 @@
 // Header inclusions, if any...
+#include <cstring>
 
 #include "gemm.h"
 
-// Using declarations, if any...
-
+const int finalSize = kI * kK;
 void GemmParallel(const float a[kI][kK], const float b[kK][kJ],
-                  float c[kI][kJ]) {
-  // Your code goes here...
+                  float c[kI][kJ])
+{
+  int k, j;
+  float *cp = (float *)c;
+  std::memset(cp, 0, sizeof(float) * kJ * kI);
+#pragma omp parallel for private(k, j)
+  for (int i = 0; i < kI; ++i)
+  {
+    for (k = 0; k < kK; ++k)
+    {
+#pragma omp simd
+      for (j = 0; j < kJ; ++j)
+      {
+        c[i][j] += a[i][k] * b[k][j];
+      }
+    }
+  }
 }

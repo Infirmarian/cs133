c2c41093a1e3b74f2338d248eacfcdb58672f658
Robert Geil <robert@rgeil.net>
2020-02-06 04:50:35 +0000

    Added recalling the data, fixed some bugs

--- lab2/mpi.cpp
+++ lab2/mpi.cpp
@@ -20,6 +20,19 @@ void Gemm(const float *a, const float *b, float *c, int I, int J, int K)
     }
   }
 }
+void GemmT(const float *a, const float *b, float *c, int I, int J, int K)
+{
+  for (int i = 0; i < I; i++)
+  {
+    for (int j = 0; j < J; j++)
+    {
+      for (int k = 0; k < K; k++)
+      {
+        c[i * I + j] += a[i * I + k] * b[j * J + k];
+      }
+    }
+  }
+}
 
 void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
                          float c[kI][kJ])
@@ -41,6 +54,7 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
   //  std::cout << "Vertical chunk size: " << verticalChunkSize << "\nHorizontal chunk size: " << horizontalChunkSize << std::endl;
   float *A = (float *)aligned_alloc(64, sizeof(float) * horizontalChunkSize * kI);
   float *B = (float *)aligned_alloc(64, sizeof(float) * verticalChunkSize * kJ);
+  float *C = (float *)aligned_alloc(64, sizeof(float) * horizontalChunkSize * verticalChunkSize);
   //  std::cout << "A: " << A << std::endl;
   //  std::cout << "B: " << B << std::endl;
   if (rank == ROOT)
@@ -56,25 +70,49 @@ void GemmParallelBlocked(const float a[kI][kK], const float b[kK][kJ],
       int hoffset = horizontalChunkSize * (i / verticalCount);
       int voffset = verticalChunkSize * (i / horizontalCount);
       std::cout << "H offset: " << hoffset << " V offset: " << voffset << std::endl;
-      MPI_Send(A + (kK * hoffset), horizontalChunkSize, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
+      MPI_Send(A + (kK * hoffset), horizontalChunkSize * kK, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
       // Permute the B matrix to send data
-
+      for (int j = 0; j < verticalChunkSize; j++)
+      {
+        for (int k = 0; k < kK; k++)
+        {
+          B[j * kK + k] = b[j][k];
+        }
+      }
+      MPI_Send(B, verticalChunkSize * kK, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
       std::cout << "Sent to " << i << std::endl;
     }
     std::cout << "Transmitted all data\n";
-    Gemm((float *)a, (float *)b, (float *)c, kI, kJ, kK);
-    std::cout << "Finished calculations" << std::endl;
+    float *newB = (float *)aligned_alloc(64, sizeof(float) * kK * kJ);
+    for (int k = 0; k < kK; k++)
+    {
+      for (int j = 0; j < kJ; j++)
+      {
+
+        newB[j * kJ + k] = b[k][j];
+      }
+    }
+    GemmT((float *)a, (float *)newB, (float *)c, kI, kJ, kK);
   }
   else
   {
     //    std::cout << "Receiving\n";
     MPI_Status s;
-    MPI_Recv(A, horizontalChunkSize, MPI_FLOAT, ROOT, 0, MPI_COMM_WORLD, &s);
-    //    MPI_Recv(B, verticalChunkSize, MPI_FLOAT, ROOT, 0, MPI_COMM_WORLD, &s);
-    float *C = (float *)aligned_alloc(64, sizeof(float) * horizontalChunkSize * verticalChunkSize);
-    //    Gemm(A, B, C, )
-    free(C);
+    MPI_Recv(A, horizontalChunkSize * kK, MPI_FLOAT, ROOT, 0, MPI_COMM_WORLD, &s);
+    MPI_Recv(B, verticalChunkSize * kK, MPI_FLOAT, ROOT, 0, MPI_COMM_WORLD, &s);
+    GemmT(A, B, C, horizontalChunkSize, verticalChunkSize, kK);
+    std::cout << "Done calculating from " << rank << std::endl;
+    MPI_Send(C, horizontalChunkSize * verticalChunkSize, MPI_FLOAT, ROOT, 0, MPI_COMM_WORLD);
   }
-  free(A);
-  free(B);
+  if (rank == ROOT)
+  {
+    for (int i = 1; i < size; i++)
+    {
+      MPI_Status s;
+      MPI_Recv(C, horizontalChunkSize * verticalChunkSize, MPI_FLOAT, i, 0, MPI_COMM_WORLD, &s);
+      std::cout << "Received from " << i << std::endl;
+    }
+  }
+  // free(A);
+  // free(B);
 }
